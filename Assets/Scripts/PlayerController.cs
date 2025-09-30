73c251d537e1060187e77bb6c093aac95b16920a
Mathilde Lawaetz <mathilde.lawaetz@gmail.com>
2025-09-30 11:12:22 +0200

    Revert "Jump Mechanic fixed"
    This reverts commit d87f44a621ca633e412bd69cc490bfa64b8ca520.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D body;
     public float speed = 5f;
     public float jumpForce = 5f;
-    public bool isJumping;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-
-        isJumping = false;
     }
 
     private void Update()
@@ -31,7 +28,7 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.A)) move = -1f;
             if (Input.GetKey(KeyCode.D)) move = 1f;
 
-            if (!isJumping && Input.GetKeyDown(KeyCode.W)) Jump();
+            if (Input.GetKeyDown(KeyCode.W)) Jump();
 
         }
         else if (playerNumber == 2)
@@ -39,35 +36,14 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftArrow)) move = -1f;
             if (Input.GetKey(KeyCode.RightArrow)) move = 1f;
 
-            //if (Input.GetKeyDown(KeyCode.UpArrow)) Jump();
+            if (Input.GetKeyDown(KeyCode.UpArrow)) Jump();
         }
 
         body.linearVelocity = new Vector2(move * speed, body.linearVelocity.y);
 
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Platform")
-        {
-            isJumping = false;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Platform")
-        {
-            isJumping = true;
-        }
-    }
-
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
-        }
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
     }
-        
 }
